09c04e65b78110bb61374a19ab80e1c488c73f5e
Chieh Kao <chieh.kao1125@gmail.com>
2024-08-11 20:05:26 +0800

    Add files via upload

---
+++ ETL/TRTC_AutoDownloadFile.R
@@ -0,0 +1,22 @@
+#### PACKAGES ----
+##install.packages("tidyr")
+##install.packages("readODS")
+##install.packages("lubridate")
+
+library(tidyr)
+library(readODS)
+library(lubridate)
+
+#### DOWNDLOAD DATA OF LAST MONTH ----
+LASTMONTH <- format(ymd(Sys.Date())-months(1), "%Y%m")
+URL <- paste("https://web.metro.taipei/RidershipPerStation/", LASTMONTH, "_cht.ods", sep = "")
+DEST <- paste("/YOUR/FILE/PATH/", LASTMONTH, "_cht.ods", sep = "")
+
+## REMOVE FIRST
+DATALASTMONTH <- readODS::read_ods(DEST, sheet = 1)
+if(exists("DATALASTMONTH")) 
+  {file.remove(DEST)}
+
+## DOWNLOAD AFTER
+download.file(URL, DEST, mode = "wb")
+
