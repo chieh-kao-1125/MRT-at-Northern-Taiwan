ed0c8a0b7f9173e830768130b164e1873d28c0dc
Chieh Kao <chieh.kao1125@gmail.com>
2024-08-13 09:59:26 +0800

    Remove some alters.R

--- Data processing/TRTC_CodeInPBI.R
+++ Data processing/TRTC_CodeInPBI.R
@@ -14,19 +14,6 @@ DATA <- lapply(files, function(x) {
 })
 TRTC_OUT <- do.call(rbind, DATA)
 
-## alter to make it readable for Power BI
-TRTC_OUT <- tidyr::drop_na(TRTC_OUT, VALUE)
-
-## also alter, but may need to be checked if new stations start operating
-TRTC_OUT$STATION <- replace(TRTC_OUT$STATION, TRTC_OUT$STATION == "板橋", "BL板橋")
-TRTC_OUT$STATION <- replace(TRTC_OUT$STATION, TRTC_OUT$STATION == "O頭前庄", "頭前庄")
-TRTC_OUT$STATION <- replace(TRTC_OUT$STATION, TRTC_OUT$STATION == "O景安", "景安")
-TRTC_OUT$STATION <- replace(TRTC_OUT$STATION, TRTC_OUT$STATION == "G大坪林", "大坪林")
-TRTC_OUT$STATION <- replace(TRTC_OUT$STATION, TRTC_OUT$STATION == "臺大醫院", "台大醫院")
-TRTC_OUT$STATION <- replace(TRTC_OUT$STATION, substr(TRTC_OUT$STATION, 1, 5) == "台北101", "台北101/世貿")
-TRTC_OUT$STATION <- replace(TRTC_OUT$STATION, TRTC_OUT$STATION == "紅樹林", "R紅樹林")
-TRTC_OUT$STATION <- replace(TRTC_OUT$STATION, TRTC_OUT$STATION == "十四張", "Y十四張")
-
 ## IN ----
 ## read all files and make a new one
 files <- list.files(path="/YOUR/FILE/PATH/", pattern = "*.ods"
@@ -39,20 +26,6 @@ DATA <- lapply(files, function(x) {
 })
 TRTC_IN <- do.call(rbind, DATA)
 
-## alter to make it readable for Power BI
-TRTC_IN <- tidyr::drop_na(TRTC_IN, VALUE)
-
-## also alter, but may need to be checked if new stations start operating
-TRTC_IN$STATION <- replace(TRTC_IN$STATION, TRTC_IN$STATION == "板橋", "BL板橋")
-TRTC_IN$STATION <- replace(TRTC_IN$STATION, TRTC_IN$STATION == "O頭前庄", "頭前庄")
-TRTC_IN$STATION <- replace(TRTC_IN$STATION, TRTC_IN$STATION == "O景安", "景安")
-TRTC_IN$STATION <- replace(TRTC_IN$STATION, TRTC_IN$STATION == "G大坪林", "大坪林")
-TRTC_IN$STATION <- replace(TRTC_IN$STATION, TRTC_IN$STATION == "臺大醫院", "台大醫院")
-TRTC_IN$STATION <- replace(TRTC_IN$STATION, substr(TRTC_IN$STATION, 1, 5) == "台北101", "台北101/世貿")
-TRTC_IN$STATION <- replace(TRTC_IN$STATION, TRTC_IN$STATION == "紅樹林", "R紅樹林")
-TRTC_IN$STATION <- replace(TRTC_IN$STATION, TRTC_IN$STATION == "十四張", "Y十四張")
-
 ## merge IN & OUT ----
 TRTC_DATA <- merge(TRTC_OUT, TRTC_IN, by = c("DATA_DATE", "STATION"), all = TRUE)
 colnames(TRTC_DATA)[3:4] <- c("OUT", "IN")
-TRTC_DATA$STATION <- iconv(TRTC_DATA$STATION, to = "UTF-8")
