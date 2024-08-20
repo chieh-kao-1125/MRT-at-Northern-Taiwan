711a1e5c110bd4cecca913b6224b0bdd95e96e2c
Chieh Kao <chieh.kao1125@gmail.com>
2024-08-20 10:24:08 +0800

    Update TRTC_AutoDownloadFile.R

--- Automatic download/TRTC_AutoDownloadFile.R
+++ Automatic download/TRTC_AutoDownloadFile.R
@@ -12,11 +12,12 @@ LASTMONTH <- format(ymd(Sys.Date())-months(1), "%Y%m")
 URL <- paste("https://web.metro.taipei/RidershipPerStation/", LASTMONTH, "_cht.ods", sep = "")
 DEST <- paste("/YOUR/FILE/PATH/", LASTMONTH, "_cht.ods", sep = "")
 
-## REMOVE FIRST
-DATALASTMONTH <- readODS::read_ods(DEST, sheet = 1)
-if(exists("DATALASTMONTH")) 
-  {file.remove(DEST)}
-
-## DOWNLOAD AFTER
-download.file(URL, DEST, mode = "wb")
+## REMOVE FIRST, DOWNLOAD AFTER
+##DATALASTMONTH <- readODS::read_ods(DEST, sheet = 1)
+if(exists("DATALASTMONTH")) {
+  file.remove(DEST)
+  download.file(URL, DEST, mode = "wb")
+} else {
+    download.file(URL, DEST, mode = "wb")
+  }
 
